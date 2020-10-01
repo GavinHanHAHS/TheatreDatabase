861124040122761a9e3b01a94a65f1bbf49ff275
GavinHanHAHS <55220804+GavinHanHAHS@users.noreply.github.com>
2020-10-01 15:25:53 -0600

    did stuff

--- main.js
+++ main.js
@@ -1,42 +1,35 @@
 // Javascript
-"use strict";
 
-document.getElementById("content").addEventListener("click", handleClick);
-document.addEventListener("click", clickfunc);
+let items = loadStorage();
 
-function handleClick(event) {
-  if(event.target.nodeName === "BUTTON") {
-    if(event.target.id === "makeupButt") {
-      document.getElementById("makeupDiv").style.display = "block";
-      document.getElementById("makeupButt").style.backgroundColor = "darkSlateBlue";
-      document.getElementById("costumeDiv").style.display = "none";
-      document.getElementById("costumeButt").style.backgroundColor = "darkBlue";
-    } else if(event.target.id === "costumeButt") {
-      document.getElementById("makeupDiv").style.display = "none";
-      document.getElementById("makeupButt").style.backgroundColor = "darkBlue";
-      document.getElementById("costumeDiv").style.display = "block";
-      document.getElementById("costumeButt").style.backgroundColor = "darkSlateBlue";
-    }
-  } else if(event.target.nodeName === "DIV") {
-    if(event.target.classList.contains("category")) { //check and open/close first level of categories
-      if(document.getElementById("subcategory" + event.target.dataset.group).style.display == "block") {
-        document.getElementById("subcategory" + event.target.dataset.group).style.display = "none";
-      } else {
-        document.getElementById("subcategory" + event.target.dataset.group).style.display = "block";
-      }
-    } 
-  } else if(event.target.nodeName === "P") { //check and open/close first level of categories but if user clicks paragraph el
-    if(event.target.parentElement.classList.contains("category")) {
-      if(document.getElementById("subcategory" + event.target.parentElement.dataset.group).style.display == "block") {
-        document.getElementById("subcategory" + event.target.parentElement.dataset.group).style.display = "none";
-      } else {
-        document.getElementById("subcategory" + event.target.parentElement.dataset.group).style.display = "block";
-      }
-    }
+document.getElementById("submitButton").addEventListener("click", submitNewItem);
+
+function submitNewItem() {
+  let itemObj = {
+    name: document.getElementById("nameInp").value,
+    description: document.getElementById("descInp").value,
+    bigCategory: document.getElementById("bigCat").value,
+    smallCategory: document.getElementById("smallCat").value
   }
+
+  items.push(itemObj);
+
+  // add item to correct divs
+  let pEl = document.createElement("p");
+  pEl.innerHTML = `Name: ${itemObj.name} Description: ${itemObj.description}`
+  document.getElementById("subcategoryc" + itemObj.bigCategory + itemObj.smallCategory).append(pEl);
   
+
+  localStorage.setItem("items", JSON.stringify(items));
 }
 
-function clickfunc(event) {
-  console.log(event.target.nodeName);
+function loadStorage() {
+  let itemsStr = localStorage.getItem("items");
+  if(itemsStr) {
+    let items = JSON.parse(itemsStr);
+    // add every item to the correct divs and stuff.
+    return items;
+  } else {
+    return [];
+  }
 }
---
+++ mainMenu.js
@@ -0,0 +1,56 @@
+"use strict";
+
+// add event listeners for things you click
+document.getElementById("content").addEventListener("click", handleClick);
+document.getElementById("submitButt").addEventListener("click", changePage);
+document.addEventListener("click", clickfunc);
+
+function handleClick(event) { // do logic based on what you clicked
+  if(event.target.nodeName === "BUTTON") { // if clicked button, change to costumes or makeup
+    if(event.target.id === "makeupButt") {
+      document.getElementById("makeupDiv").style.display = "block";
+      document.getElementById("makeupButt").style.backgroundColor = "darkSlateBlue";
+      document.getElementById("costumeDiv").style.display = "none";
+      document.getElementById("costumeButt").style.backgroundColor = "darkBlue";
+    } else if(event.target.id === "costumeButt") {
+      document.getElementById("makeupDiv").style.display = "none";
+      document.getElementById("makeupButt").style.backgroundColor = "darkBlue";
+      document.getElementById("costumeDiv").style.display = "block";
+      document.getElementById("costumeButt").style.backgroundColor = "darkSlateBlue";
+    }
+  } else if(event.target.nodeName === "DIV") { // if clicked div, check if it's a category button and open/close
+    if(event.target.classList.contains("category")) { //check and open/close first level of categories
+      if(document.getElementById("subcategory" + event.target.dataset.group).style.display == "block") {
+        document.getElementById("subcategory" + event.target.dataset.group).style.display = "none";
+      } else {
+        document.getElementById("subcategory" + event.target.dataset.group).style.display = "block";
+      }
+    } 
+  } else if(event.target.nodeName === "P") { //check and open/close first level of categories but if user clicks paragraph el
+    if(event.target.parentElement.classList.contains("category")) {
+      if(document.getElementById("subcategory" + event.target.parentElement.dataset.group).style.display == "block") {
+        document.getElementById("subcategory" + event.target.parentElement.dataset.group).style.display = "none";
+      } else {
+        document.getElementById("subcategory" + event.target.parentElement.dataset.group).style.display = "block";
+      }
+    }
+  }
+  
+}
+
+function clickfunc(event) { //simple thing to check what you click on when you click
+  console.log(event.target.nodeName);
+}
+
+function changePage() { // change pages
+  if(document.getElementsByClassName("main")[0].style.display == "none") {
+    document.getElementsByClassName("main")[0].style.display = "block";
+    document.getElementsByClassName("submit")[0].style.display = "none";
+    document.getElementById("submitButt").innerHTML = "Enter New Item";
+  } else {
+    document.getElementsByClassName("main")[0].style.display = "none";
+    document.getElementsByClassName("submit")[0].style.display = "block";
+    document.getElementById("submitButt").innerHTML = "Return to Main Page";
+  }
+
+}
